6c604aba6148ded3ff62b21c235619d39525b986
ujjwal-2001 <149869490+ujjwal-2001@users.noreply.github.com>
2024-01-24 15:40:07 +0530

    given meaningful names

--- Lab02/main.c
+++ Lab02/main.c
@@ -3,11 +3,11 @@
 #include<stdio.h>
  
 //********************Macro for Pins and Clock**************
-# define PIN0 0x01
-# define PIN1 0x02
-# define PIN2 0x04
-# define PIN3 0x08
-# define PIN4 0x10
+#define PIN0 0x01
+#define PIN1 0x02
+#define PIN2 0x04
+#define PIN3 0x08
+#define PIN4 0x10
  
 //**********************Macro for Colors********************
 
@@ -21,11 +21,13 @@
 
 #define MAX_BLINK_DELAY 2000
 #define MIN_BLINK_DELAY 100
- 
-uint8_t SW1;
-uint8_t SW2;
+#define DEBOUNCING_DELAY DEBOUNCING_DELAY
+
+#define SW1 ((GPIO_PORTF_DATA_R & (1<<4))>>4)
+#define SW2 ((GPIO_PORTF_DATA_R & (1<<0))>>0)
+
 uint8_t delay;
-int blink_delay = 2000;
+int blink_delay = MAX_BLINK_DELAY;
 int select_color = 0;
 uint8_t color = COLOR_GREEN_ON;
 int flag = 0;
@@ -46,20 +48,16 @@ while(1)
 {
     if (flag==0){
         GPIO_PORTF_DATA_R = color;
-        delayMs(blink_delay);
+        delayMs_check(blink_delay);
         GPIO_PORTF_DATA_R = 0x00;
-        delayMs(blink_delay);
+        delayMs_check(blink_delay);
     }
- 
- 
-    SW1 = ((GPIO_PORTF_DATA_R & (1<<4))>>4); // Read PF4 into SW1
-    SW2 = ((GPIO_PORTF_DATA_R & (1<<0))>>0); // Read PF0 into SW2
  
     if(SW1 == 0)
     {
         select_color = (select_color + 1)%7;
         update_color();
-        delay_nor(70);
+        delayMs(DEBOUNCING_DELAY);
     }
  
     if(SW2 == 0)
@@ -68,14 +66,14 @@ while(1)
                 {
                     blink_delay = blink_delay/2;
                     flag=0;
-                    delay_nor(70);
+                    delayMs(DEBOUNCING_DELAY);
                 }
                 else
                 {
                     blink_delay = MAX_BLINK_DELAY;
                     GPIO_PORTF_DATA_R = color;
                     flag=1;
-                    delay_nor(70);
+                    delayMs(DEBOUNCING_DELAY);
                 }
         }
 }
@@ -107,47 +105,46 @@ void update_color(){
         }
 }
 
-void delay_nor(int n)
+void delayMs(int n)
 {
 int i, j;
 for(i = 0 ; i < n; i++)
 for(j = 0; j < 3180; j++) {} /* do nothing for 1 ms */
 }
  
-void delayMs(int n)
+void delayMs_check(int n)
 {
 int i, j;
-for(i = 0 ; i < n; i++)
-{
-       SW1 = ((GPIO_PORTF_DATA_R & (1<<4))>>4); 
-       SW2 = ((GPIO_PORTF_DATA_R & (1<<0))>>0);
+    for(i = 0 ; i < n; i++)
+    {
  
        if(SW1 == 0)
        {
             select_color = (select_color + 1)%7;
             update_color();
-            delay_nor(70);
+            delayMs(DEBOUNCING_DELAY);
             break;
        }
  
-           if(SW2== 0)
-           {
-                   if(blink_delay > MIN_BLINK_DELAY)
-                {
-                    blink_delay = blink_delay/2;
-                    flag=0;
-                    delay_nor(70);
-                }
-                else
-                {
-                    blink_delay = MAX_BLINK_DELAY;
-                    GPIO_PORTF_DATA_R = color;
-                    flag=1;
-                    delay_nor(70);
-                }
-                delay_nor(70);
-                break;
-           }
-for(j = 0; j < 3180; j++){} /* do nothing for 1 ms */
-}
+        if(SW2== 0)
+        {
+                if(blink_delay > MIN_BLINK_DELAY)
+            {
+                blink_delay = blink_delay/2;
+                flag=0;
+                delayMs(DEBOUNCING_DELAY);
+            }
+            else
+            {
+                blink_delay = MAX_BLINK_DELAY;
+                GPIO_PORTF_DATA_R = color;
+                flag=1;
+                delayMs(DEBOUNCING_DELAY);
+            }
+            delayMs(DEBOUNCING_DELAY);
+            break;
+        }
+
+        for(j = 0; j < 3180; j++){} /* do nothing for 1 ms */
+    }
 }
